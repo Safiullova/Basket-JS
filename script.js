d47b649305bb545ca9a6a5a5ff876b5a5cc1581b
Safiullova <e.safiullova2000@mail.ru>
2023-04-05 17:03:39 +0400

    oh

--- script.js
+++ script.js
@@ -3,12 +3,22 @@
 function summ (){
     let sum = parseFloat(document.querySelector('.price').textContent) * parseFloat(document.querySelector('.number').textContent); 
 
-    document.querySelector('.basket__calc-text').textContent = String(sum);
+    document.querySelector('.basket__calc-text').textContent = sum;
 }
 summ();
 
 
-const card =  document.querySelectorAll('.basket__calc-text').textContent;
+const card =  document.querySelectorAll('.basket__calc-text');
 
 console.log(card);
-// let total = 
+
+let total = Number(card[0].textContent) + Number(card[1].textContent) + Number(card[2].textContent) + Number(card[3].textContent)
+
+console.log(total);
+
+document.querySelector('.result__total').textContent = total;
+
+function sale () {
+    let sales = Number(total)*0.8;
+    document.querySelector('.result__total').textContent= sales;
+}
---
+++ with stars/stars.js
@@ -0,0 +1,5 @@
+let a = document.querySelector('#add').value;
+console.log(a);
+
+let calc () = a**2;
+console.log(calc);
