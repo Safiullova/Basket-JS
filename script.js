cde1cda60ed4b84198a838b3962a4909f431d048
Safiullova <e.safiullova2000@mail.ru>
2023-04-06 14:04:18 +0400

    finish homework

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
+let price = document.querySelectorAll('.price');
+console.log(price);
+let number = document.querySelectorAll('.number');
+console.log(number);
 
+let card =  document.querySelectorAll('.basket__calc-text');
 
-function summ (){
-    let sum = parseFloat(document.querySelector('.price').textContent) * parseFloat(document.querySelector('.number').textContent); 
-
-    document.querySelector('.basket__calc-text').textContent = sum;
+summ = (price, number) => {
+    sum = price* number; 
+return sum;
 }
-summ();
 
+let apple = card[0].textContent = summ(Number(price[0].textContent), Number(number[0].textContent));
+let pear = card[1].textContent = summ(Number(price[1].textContent), Number(number[1].textContent));
+let fruit = card[2].textContent = summ(Number(price[2].textContent), Number(number[2].textContent));
+let oranges = card[3].textContent = summ(Number(price[3].textContent), Number(number[3].textContent));
 
-const card =  document.querySelectorAll('.basket__calc-text');
-
-console.log(card);
-
-let total = Number(card[0].textContent) + Number(card[1].textContent) + Number(card[2].textContent) + Number(card[3].textContent)
-
-console.log(total);
-
+const total = apple + pear + fruit + oranges;
 document.querySelector('.result__total').textContent = total;
 
-function sale () {
-    let sales = Number(total)*0.8;
-    document.querySelector('.result__total').textContent= sales;
-}
+const element = document.getElementById('btn');
+
+element.addEventListener('click', sale = () => {
+    card[0].textContent = apple *= 0.8;
+    card[1].textContent = pear *= 0.8;
+    card[2].textContent = fruit *= 0.8;
+    card[3].textContent = oranges *= 0.8;
+    document.querySelector('.result__total').textContent = total*0.8;
+},{ once: true });
--- with stars/stars.js
+++
@@ -1,5 +0,0 @@
-let a = document.querySelector('#add').value;
-console.log(a);
-
-let calc () = a**2;
-console.log(calc);
